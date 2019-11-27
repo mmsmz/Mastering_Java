89eb759ff14c8d64b7cd93f178d0254e072698f9
arafath <marafath07@gmail.com>
2019-11-28 00:18:07 +0530

    Updating with Lamda Expression..

--- src/MJ20_LamdaExpression/M1_.java
+++ src/MJ20_LamdaExpression/M1_.java
@@ -1,4 +1,26 @@
 package MJ20_LamdaExpression;
 
+import java.util.stream.Stream;
+
 public class M1_ {
+    public static void main(String[] args) {
+        // already function programming was there in scala, hascle, c# but only in java 8 it was introduce to java
+        // we have three features main features in java 8
+            // 1. Functional Programming
+            // 2. Lamda Expression
+            // 3. Stream API
+
+        // 1. we can define methods in the inteface.
+        // 2. functional programming -  behavior
+                // there is two techniques to code.
+                    // 1. Imperative Programming - we define : what to do and how to do it?
+                    // 2. Declarative Programming - - we define : what to do ?
+        // 3. Lamda Expression
+        // 4. Stream API - it helps Concurrency and functional programming
+
+
+        // Stream API
+            // we have certain methods of stream api and it was introduced
+    }
+
 }
